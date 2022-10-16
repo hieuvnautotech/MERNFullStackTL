4e9280613e45a9f9a35dbb294f3751aa42a4f7f6
hieu <hieuvnautotech@gmail.com>
2022-10-16 10:12:37 +0700

    nha

--- server/index.js
+++ server/index.js
@@ -1,31 +1,31 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-// import posts from "./routers/posts.js";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import posts from "./routers/posts.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const URI = process.env.DATABASE_URL;
+const URI = 'mongodb+srv://hieuvnautotech:211899900So@mern-learnit.ygpexhl.mongodb.net/MERNFullStack?retryWrites=true&w=majority'
 app.use(bodyParser.json({limit:'30mb'}));
 app.use(cors());
-// app.use(express.json({ limit: "30mb" }));
+app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 
-// app.use("/posts", posts);
+app.use("/posts", posts);
 
-// mongoose
-//   .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log("Connected to DB");
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
-//   })
-//   .catch((err) => {
-//     console.log("err", err);
-//   });
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
